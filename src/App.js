08fe769c40a3e8ed2dede6c79bceeb89b2fb4719
VedPrakash-cloud <rocky007ved@gmail.com>
2025-10-14 21:05:33 +0530

    test case update 7

--- src/App.js
+++ src/App.js
@@ -72,6 +72,7 @@ export default function App() {
           isOpen={isOpen}
           onRequestClose={closeRequest}
           ariaHideApp={false}
+          className="modal"
           style={{
             content: {
               width: "min-content",
